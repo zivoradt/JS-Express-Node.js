9ff1f54193eb25698c7b4a1b2a0aa13063ba2cdc
zivoradt <zivoradtrickovic@gmail.com>
2023-01-23 13:13:09 +0100

    changed html files to ejs

--- app.ts
+++ app.ts
@@ -33,13 +33,12 @@ app.use(function(req, res, next)
 // error handler
 app.use(function(err:createError.HttpError, req:express.Request, res:express.Response, next:express.NextFunction) 
 {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  let message = err.message;
+  let error = req.app.get('env') === 'development' ? err : {};
+  
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { message: message, error: error});
 });
 
 module.exports = app;
