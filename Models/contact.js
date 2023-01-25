0451824527ae40c1e0d734d6c242f4e733bc3411
zivoradt <zivoradtrickovic@gmail.com>
2023-01-25 22:04:43 +0100

    contact-model

--- Models/contact.js
+++ Models/contact.js
@@ -6,6 +6,8 @@ const ContactSchema = new Schema({
     FullName: String,
     EmailAddress: String,
     ContactNumber: String
+}, {
+    collection: "contact "
 });
 const Model = mongoose.model("Contact", ContactSchema);
 module.exports.Model = Model;
